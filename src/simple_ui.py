4affc5a8002c8df7e53a57145bbcf8504a8c5c96
Luís <luis.fmso.negrao@gmail.com>
2025-10-23 21:16:36 +0200

    Minor fix

--- src/simple_ui.py
+++ src/simple_ui.py
@@ -3,7 +3,6 @@ import os
 
 API_URL = 'https://xemcclumj8.execute-api.us-east-1.amazonaws.com/internet_search_assistant/internet-search-assistant'
 api_key = os.environ.get("ISA_API_KEY")
-print(api_key)
 headers = {"api-key": api_key}
 
 def call_assistant(user_message):
